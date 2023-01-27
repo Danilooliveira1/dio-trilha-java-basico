35fc229ef8bcc1f32e13c31f7976817a0e3b199f
Danilooliveira1 <danilo.r.oliveira1@gmail.com>
2023-01-27 15:07:36 -0300

    Testando variáveis

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,26 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        byte idade = 33;
+        short ano = 2023;
+        long cpf = 987554321109l;
+        float pi = 3.14f;
+        double salario = 1275.30;
+
+        System.out.println(idade);
+        System.out.println(ano);
+        System.out.println(cpf);
+        System.out.println(pi);
+        System.out.println(salario);
+
+
+        int numero =10;
+        numero = 25;
+
+        final double VALOR_PI = 3.14;
+
+        System.out.println(numero);
+        System.out.println("Valor de PI é: " + VALOR_PI);
+
     }
 }
